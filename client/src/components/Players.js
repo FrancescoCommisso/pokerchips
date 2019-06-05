8e93948639cd644b0af0b4b61489587510b99e4a
Francesco Commisso <francescocommissodev@gmail.com>
2019-06-05 16:07:40 -0400

    looks nice

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import "./Main.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Player from "./Player";
+import Switch from "react-switch";
 
 class Players extends Component {
-  state = {};
-  render() {
-    return (
-      <Container className="bordered">
-        <Row>
-          <Col>
-            <h4 className="title">Standings</h4>
-          </Col>
-        </Row>
+  state = { show: false };
 
+  onToggle = e => {
+    this.setState({ show: e });
+  };
+
+  render() {
+    let info;
+    if (this.state.show) {
+      info = (
         <Row className=" ">
           {Object.keys(this.props.players).map((player, i) => (
             <Col className=" ">
@@ -25,6 +26,30 @@ class Players extends Component {
             </Col>
           ))}
         </Row>
+      );
+    } else {
+      info = null;
+    }
+
+    return (
+      <Container className="bordered">
+        <Row>
+          <Col>
+            <h4 className="title">Standings</h4>
+          </Col>
+          <Col className="text-right">
+            <Switch
+              className="align-middle"
+              onColor="#000000"
+              checkedIcon={false}
+              uncheckedIcon={false}
+              onChange={this.onToggle}
+              checked={this.state.show}
+            />
+          </Col>
+        </Row>
+
+        {info}
       </Container>
     );
   }
